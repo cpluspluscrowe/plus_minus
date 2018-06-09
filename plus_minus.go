9c9b6e8eb35ab11cad6eb6b313d250b1a53f5b31
Chad Crowe <ccrowe@linkedin.com>
2018-06-09 12:25:25 -0500

    refactored str to int into a function

--- plus_minus.go
+++ plus_minus.go
@@ -8,6 +8,15 @@ import (
 	"strings"
 )
 
+func transformInputToIntegers(input []string) []int {
+	var transformed []int
+	for _, item := range input {
+		var number, _ = strconv.Atoi(item)
+		transformed = append(transformed, number)
+	}
+	return transformed
+}
+
 func main() {
 	reader := bufio.NewReaderSize(os.Stdin, 1024*1024)
 	_, _, _ = reader.ReadLine() // number of inputs
@@ -16,8 +25,8 @@ func main() {
 	var positive_count = 0
 	var zero_count = 0
 	var negative_count = 0
-	for _, item := range splitData {
-		var number, _ = strconv.Atoi(item)
+	var numbers = transformInputToIntegers(splitData)
+	for _, number := range numbers {
 		if number > 0 {
 			positive_count += 1
 		} else if number < 0 {
