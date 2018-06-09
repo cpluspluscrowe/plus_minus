ef04ad623be3c766dcf4f3491ff0a11e14394a5b
Chad Crowe <ccrowe@linkedin.com>
2018-06-09 13:01:46 -0500

    added another test

--- plus_minus_test.go
+++ plus_minus_test.go
@@ -10,3 +10,17 @@ func TestStringConversion(t *testing.T) {
 		t.Errorf("Transformed int slice was incorrect, got: %d, want: %d", output, expected)
 	}
 }
+
+func TestGetSignCounts(t *testing.T) {
+	input := []int{0, 0, 0, 1, -1, -2}
+	var positive_count, zero_count, negative_count int = getSignCounts(input)
+	if positive_count != 1 {
+		t.Errorf("Incorrect positive_count, got: %d, want: %d", positive_count, 1)
+	}
+	if zero_count != 3 {
+		t.Errorf("Incorrect zero_count, got: %d, want: %d", zero_count, 3)
+	}
+	if negative_count != 2 {
+		t.Errorf("Incorrect negative_count, got: %d, want: %d", negative_count, 2)
+	}
+}
