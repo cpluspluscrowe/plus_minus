84d0bcd70929f5bb4d655c0a6a937fc1a09d9ad1
Chad Crowe <ccrowe@linkedin.com>
2018-06-09 12:18:59 -0500

    success on hackerrank

--- plus_minus.go
+++ plus_minus.go
@@ -1,16 +1,33 @@
-// 6
-// -4 3 -9 0 4 1
-// print fraction of positive, negative, and zeros
 package main
 
 import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
 	reader := bufio.NewReaderSize(os.Stdin, 1024*1024)
-	reader.ReadLine()
-	fmt.Println("Hello!")
+	_, _, _ = reader.ReadLine() // number of inputs
+	var strData, _, _ = reader.ReadLine()
+	var splitData = strings.Split(string(strData), " ")
+	var positive_count = 0
+	var zero_count = 0
+	var negative_count = 0
+	for _, item := range splitData {
+		var number, _ = strconv.Atoi(item)
+		if number > 0 {
+			positive_count += 1
+		} else if number < 0 {
+			negative_count += 1
+		} else {
+			zero_count += 1
+		}
+	}
+	var total = float64(positive_count + zero_count + negative_count)
+	fmt.Println(float64(positive_count) / total)
+	fmt.Println(float64(negative_count) / total)
+	fmt.Println(float64(zero_count) / total)
 }
